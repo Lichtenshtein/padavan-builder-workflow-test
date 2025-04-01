3c14b7a4f0e117b272744643558c7b5e26dd50e0
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-04-01 13:41:08 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,6 @@
+# Отключение log wireguard debug
+sed -i 's/func_enable_kernel_param "CONFIG_WIREGUARD_DEBUG"/func_disable_kernel_param "CONFIG_WIREGUARD_DEBUG"/' padavan-ng/trunk/build_firmware.sh
+
 busybox_cfg="padavan-ng/trunk/configs/boards/busybox.config"
 
 busybox_enable() {
