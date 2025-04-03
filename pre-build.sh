c37d2acdc88cc81c6a86ff6eb162c4fa39173109
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-04-03 11:42:44 +0000

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,6 +3,18 @@
 # Отключение log wireguard debug
 sed -i 's/func_enable_kernel_param "CONFIG_WIREGUARD_DEBUG"/func_disable_kernel_param "CONFIG_WIREGUARD_DEBUG"/' padavan-ng/trunk/build_firmware.sh
 
+# Change building server timezone 
+ln -snf /usr/share/zoneinfo/Europe/Astrakhan /etc/localtime
+apt update
+DEBIAN_FRONTEND=noninteractive apt install tzdata -y
+
+# Wi-Fi Country Code
+sed -i 's/UA/RU/g' padavan-ng/trunk/user/shared/defaults.h
+
+# NTP servers
+sed -i 's/pool.ntp.org/ru.pool.ntp.org/' padavan-ng/trunk/user/shared/defaults.h
+sed -i 's/time.in.ua/time.nist.gov/' padavan-ng/trunk/user/shared/defaults.h
+
 busybox_cfg="padavan-ng/trunk/configs/boards/busybox.config"
 
 busybox_enable() {
